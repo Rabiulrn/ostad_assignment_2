ebdcd35da365819f94bb8f07ad34d7403aa557d5
Rabiulrn <rnrabiul@gmail.com>
2023-09-20 18:51:43 +0600

    tast4 done

---
+++ task4.php
@@ -0,0 +1,15 @@
+<?php
+function printFibonacci($n) {
+    $first = 0;
+    $second = 1;
+
+    for ($i = 0; $i < $n; $i++) {
+        echo $first . " ";
+
+        $next = $first + $second;
+        $first = $second;
+        $second = $next;
+    }
+}
+
+printFibonacci(15);
